54f0cd80834df04e7079152fe1e558d287a152f0
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-01-03 16:32:16 -0500

    Breadcrumbs using the react-router

---
+++ src/components/Breadcrumbs.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function Breadcrumbs() {
+  const location = useLocation();
+  console.log(location);
+
+  //goal is to output a template like:
+  //if the pathname property of the location is '/help/contact' --> help | contact
+  let currentLink = "";
+
+  const crumbs = location.pathname
+    .split("/")
+    .filter((crumb) => crumb !== "")
+    .map((crumb) => {
+      currentLink += `/${crumb}`;
+
+      return (
+        <div className="crumb" key={crumb}>
+          <Link to={currentLink}>{crumb}</Link>
+        </div>
+      );
+    });
+  console.log(crumbs);
+
+  return <div className="breadcrumbs">{crumbs}</div>;
+}
+
+export default Breadcrumbs;
